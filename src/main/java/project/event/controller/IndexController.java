79eadb40ae0351af7e4cc3fd82f8ad29366d0c08
Marek Tomasik <marek-tomas@wp.pl>
2018-03-08 12:57:25 +0100

    First commit

---
+++ src/main/java/project/event/controller/IndexController.java
@@ -0,0 +1,18 @@
+package project.event.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+
+	@RequestMapping("/index")
+	public String index1() {
+		return "index";
+	}
+}
